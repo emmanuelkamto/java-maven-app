7b3260fe52928328b4883e3d748bce8b570134cc
emmanuelkamto <56613364+emmanuelkamto@users.noreply.github.com>
2023-04-19 16:46:17 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,57 +1,15 @@
-def gv
+def buildApp() {
+    echo 'building the application...'
+}
 
-pipeline {
-    agent any
-    parameters {
-        choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: '')
-        booleanParam(name: 'executeTests', defaultValue: true, description: '')
-    }
+def testApp() {
+    echo 'testing the application...'
+}
 
-    stages {
-        stage("init") {
-            steps {
-                script {
-                    gv = load "script.groovy"
-                }
-            }
-        }
-        stage("build") {
-            steps {
-                script {
-                    gv.buildApp()
-                }
-            }
-        }
-           }
-        stage("test") {
-            when {
-                expression {
-                    params.executeTests
-                }
-            }
-            steps {
-                script {
-                    gv.testdApp()
-                }
-            }
-        }
-           }
-        stage("deploy") {
-            steps {
-                script {
-                    gv.deployApp()
-                }
-            }
-        }
-            
-            }
-        }
-           }
-        stage("deploy") {
-            steps {
-                script {
-                    gv.deployApp()
-                }
-            }
-        }
+def deployApp() {
+    echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
+}
+
+return this
             
